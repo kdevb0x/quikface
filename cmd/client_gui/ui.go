81b6f86557971b2ada841e4fb954f9e3c3794f7a
Kristofer Davidson <kdevb0x@gmail.com>
2019-09-30 22:05:22 -0700

    begin client gui

---
+++ cmd/client_gui/ui.go
@@ -0,0 +1,21 @@
+// Copyright 2019 kdevb0x Ltd. All rights reserved.
+// Use of this source code is governed by the BSD 3-Clause license
+// The full license text can be found in the LICENSE file.
+
+package main
+
+import (
+	"image"
+	"image/draw"
+	"runtime"
+
+	"github.com/andlabs/ui"
+)
+
+type video_viewport struct {
+	imgwidget *ui.Image
+}
+
+func ExecOnNewThread(f func(...interface{}) error ), retRef bool) opts OptCallback {
+
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module quikface
 go 1.13
 
 require (
+	github.com/andlabs/ui v0.0.0-20180902183112-867a9e5a498d
 	github.com/gobwas/httphead v0.0.0-20180130184737-2c6c146eadee // indirect
 	github.com/gobwas/pool v0.2.0 // indirect
 	github.com/gobwas/ws v1.0.2
