ad549a0ad111692db5f89010847509ddcb39cfd0
Kristofer Davidson <kdevb0x@gmail.com>
2019-10-10 12:46:48 -0700

    promote internal package

--- go.mod
+++ go.mod
@@ -3,12 +3,17 @@ module github.com/kdevb0x/quikface
 go 1.13
 
 require (
+	github.com/dennwc/dom v0.3.0
 	github.com/gobwas/httphead v0.0.0-20180130184737-2c6c146eadee // indirect
 	github.com/gobwas/pool v0.2.0 // indirect
 	github.com/gobwas/ws v1.0.2
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/mux v1.7.3
+	github.com/notedit/janus-go v0.0.0-20180821162543-a152adf0cb7b // indirect
+	github.com/pion/example-webrtc-applications v0.0.0-20190919072239-e187587c5b97 // indirect
 	github.com/pion/webrtc/v2 v2.1.6
+	github.com/povilasv/prommod v0.0.12 // indirect
+	github.com/prometheus/client_golang v1.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20191002192127-34f69633bfdc
 	golang.org/x/net v0.0.0-20191007182048-72f939374954
 )
---
+++ jsCamera.go
@@ -0,0 +1,82 @@
+// Copyright 2019 kdevb0x Ltd. All rights reserved.
+// Use of this source code is governed by the BSD 3-Clause license
+// The full license text can be found in the LICENSE file.
+
+// +build wasm,js
+
+package internal // import "github.com/kdevb0x/quikface/internal"
+
+import (
+	"io"
+	gojs "syscall/js"
+
+	"github.com/dennwc/dom"
+	"github.com/dennwc/dom/js"
+	"github.com/dennwc/dom/net/webrtc"
+
+	rtc "github.com/pion/webrtc/v2"
+)
+
+var (
+	_ webrtc.Local
+)
+
+type MediaStream interface {
+	// returns the instances unique 36 char string
+	Id() string
+	GetAudioTracks []MediaStreamTrack
+	GetTrackById(id string) (MediaStreamTrack, error)
+	GetVideoTracks() []MediaStreamTrack
+
+}
+
+// JSAudioTrack represents an audio track from the browsers MediaStream.
+type JSAudioTrack struct {
+	js.Value
+}
+
+// JSVideoTrack represents a video track from the browsers MediaStream.
+type JSVideoTrack struct {
+	js.Value
+}
+
+func (au *JSAudioTrack) Id() string {
+
+}
+
+// InitBrowserCam instantiates the webcam through the browsers
+// navigator.MediaDevices.getUserMedia API.
+func InitBrowserCam() (MediaStream, error) {
+	var localVideo = dom.GetDocument().QuerySelector("localVideo")
+	goconstraints := map[string]interface{}{"audio": true, "video": map[string]interface{}{"facingMode": "user"}}
+	constraints := js.ValueOf(goconstraints)
+	mediaDevices := gojs.Global().Get("navigator").Get("mediaDevices")
+
+	getUserMediaPromise := mediaDevices.Call("getUserMedia", constraints)
+
+	js.AsyncCallbackOf(func(streams []js.Value) {
+		if len(streams) > 0 {
+			for _, stream := range streams {
+				var vidTrack = stream.Call("getVideoTracks")
+			}
+			localVideo.SetAttribute("srcObject", stream)
+		}
+
+	}
+	// var mediaPromise = js.NewPromise(func()([]interface{}, error) {
+	// 	return mediaDevices.Call("getUserMedia", constraints), nil
+	// })
+
+	var rtcConfig = rtc.Configuration{
+		ICEServers: []rtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+	peerconn, err := rtc.NewPeerConnection(rtcConfig)
+	if err != nil {
+		return nil, err
+	}
+	peerconn.
+}
